626996f22d66bb47a23ee7e804c6b1f00874b4fa
Daniel Cairoli <daniel.cairoli@icloud.com>
2026-08-05 08:22:48 +0200

    inference angepasst für 500 steps

--- inference.py
+++ inference.py
@@ -4,5 +4,6 @@ from diffusers import DDPMPipeline
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
 pipe = DDPMPipeline.from_pretrained("randomart_pipeline").to(device)
-image = pipe(num_inference_steps=100).images[0]
-image.save("generated.png")
+for i in range(3):
+    image = pipe(num_inference_steps=500).images[0]
+    image.save(f"generated/generated-500_{i+1}.png")
